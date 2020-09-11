3a019dc9f5fdc4e99e6542cb4af939d78af09acd
TomFram <tomfram03@gmail.com>
2020-09-10 23:11:16 -0300

    com

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <h1> Primeiro Commit ReactJS</h1>
         </p>
         <a
           className="App-link"
