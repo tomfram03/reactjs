78a0d631a550194122de0f124d4f41189799b518
TomFram <tomfram03@gmail.com>
2020-09-11 15:22:55 -0300

    header

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Header from './header';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           <h1> Primeiro Commit ReactJS</h1>
           <p>Teste de Commit</p>
           <p>Um commit pra teste</p>
+
         </p>
         <a
           className="App-link"
