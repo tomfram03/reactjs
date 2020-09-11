4c4402e168af67b8d5b6eb6559d6b7f71af1fa04
TomFram <tomfram03@gmail.com>
2020-09-10 23:04:05 -0300

    Updates

---
+++ precache-manifest.de44977ffa7fbd5874b6580d41eb8555.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74e048ff4639f9e27c4f680e2ac65b88",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "d5debb9a4765f9a69fae",
+    "url": "/react-gh-pages/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e48ef8872e7516225a9f",
+    "url": "/react-gh-pages/static/js/2.c02f2d55.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-gh-pages/static/js/2.c02f2d55.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5debb9a4765f9a69fae",
+    "url": "/react-gh-pages/static/js/main.580058bd.chunk.js"
+  },
+  {
+    "revision": "c8070daef43b0e5e4a14",
+    "url": "/react-gh-pages/static/js/runtime-main.be2fa00e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-gh-pages/static/media/logo.5d5d9eef.svg"
+  }
+]);
